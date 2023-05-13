15c04ae22d6775b894ecc74800c07ee1fe711230
Guna P <111977753+Guna-Pandi@users.noreply.github.com>
2023-05-13 15:43:11 +0530

    Data Fetching

--- src/weather.jsx
+++ src/weather.jsx
@@ -21,32 +21,66 @@ import {
 
 import { TbTemperatureCelsius } from "react-icons/tb";
 
-import { ImSpinner8 } from "react-icons/im";
+import { BounceLoader } from "react-spinners";
 
-// 017da455c9f0fe6e776dd56c42733b57
-
-const APIkey = '017da455c9f0fe6e776dd56c42733b57';
-axios.defaults.headers.common["Authorization"] = `Bearer ${APIkey}`;
-
+const APIkey = "06fa6983532b88fed1776013fb6650aa";
 
 const Weather = () => {
+  const [data, setData] = useState(null);
+  const [location, setLocation] = useState("Namakkal");
 
-  const [data,setData] = useState(null);
-  const [location,setLocation] = useState('Namakkal')
-
-  useEffect(()=>{
-    const url=`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIkey}`;
+  useEffect(() => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIkey}`;
     axios.get(url).then((res) => {
       setData(res.data);
     });
-  },[location]);
-console.log(data);
+  }, [location]);
 
-  return (
-  <div className=" font-bold w-1 h-10 ">
-  weather
-  </div>
-  )
+  if (!data) {
+    return (
+      <div>
+        <div>
+          <BounceLoader className=" text-5xl" />
+        </div>
+      </div>
+    );
+  }
+
+  let icon;
+
+  console.log(data.weather[0].main);
+
+  switch (data.weather[0].main) {
+    case "Clouds":
+      icon = <IoMdCloudy />;
+      break;
+
+    case "Haze":
+      icon = <BsCloudHaze2Fill />;
+      break;
+
+    case "Rain":
+      icon = <IoMdRainy />;
+      break;
+
+    case "Clear":
+      icon = <IoMdSunny />;
+      break;
+
+    case "Drizzle":
+      icon = <BsCloudDrizzleFill />;
+      break;
+
+    case "Snow":
+      icon = <IoMdSnow />;
+      break;
+
+    case "Thunderstrome":
+      icon = <IoMdThunderstorm />;
+      break;
+  }
+
+  return <div className=" font-bold w-1 h-10  text-6xl">{icon}</div>;
 };
 
 export default Weather;
